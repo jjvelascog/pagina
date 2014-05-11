d0cc709ad614b1fd231a2674ee74ad46dedca2cf
arturonatho <arturonatho@gmail.com>
2014-05-11 17:25:25 -0400

    Codigo Vtiger
    Mi codigo

--- lib/vtiger.rb
+++ lib/vtiger.rb
@@ -1,25 +1,25 @@
 class Vtiger
-  require "rubygems"
-  require "httparty"
 
 def self.get_address_from_rut(rut)
 
     user_name = 'grupo4'
-    user_key = 'cHjPQ6N4cB87PR1D'
-    url1 = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php'
-    #consulta = HTTParty.get(url1+'?operation=getchallenge&username='+user_name)
-    consulta = HTTParty.get(url1,:query => {"operation" => "getchallenge","username" => user_name})
-    token = consulta["result"]["token"]
-    
-    md5 = Digest::MD5.hexdigest(token+user_key)
-    
-    #consulta2 = HTTParty.post(url1+'?operation=login&username='+user_name+'&accessKey='+md5)
-    consulta2 = HTTParty.post(url1,:query => {"operation" => "login","username" => user_name,"accessKey" => md5})
-    puts consulta
-    puts token
-    puts md5
-    puts consulta2
+    url1 = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=getchallenge'
+    token = JSON.parse(RestClient.get url1, :body => {
+        username: user_name
+    })['result']['token']
+
+    url2 = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=login'
+    digest_string = token+'cHjPQ6N4cB87PR1D'
+    md5 = Digest::MD5.hexdigest(digest_string)
+    response = RestClient.post url2, :body => {
+        'operation' => 'login',
+        'username' => user_name,
+        'accessKey' => md5
+    }
 
 
+
+    return response
+    #return "Token: " + token + " MD5: " + md5
   end
 end
