b16a111261501e16b339c7945ef1ad0230d426f3
arturonatho <arturonatho@gmail.com>
2014-05-13 15:16:46 -0400

    Funcion Vtiger

--- lib/vtiger.rb
+++ lib/vtiger.rb
@@ -1,25 +1,24 @@
 class Vtiger
 
-def self.get_address_from_rut(rut)
-
+  def self.get_address_from_rut(rut)
+    #Primero solicitamos token a API Vtiger
     user_name = 'grupo4'
-    url1 = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=getchallenge'
-    token = JSON.parse(RestClient.get url1, :body => {
-        username: user_name
-    })['result']['token']
+    token = JSON.parse((RestClient.get 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=getchallenge&username=grupo4').body)['result']['token']
+    url = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=login'
+    md5 = Digest::MD5.hexdigest(token+'cHjPQ6N4cB87PR1D')
+    #Encriptamos token recibido + accessKey y recibimos sessionId después del login
+    response = RestClient.post url, 
+         'operation' => 'login',
+         'username' => user_name,
+         'accessKey' => md5         
+    sessionid= JSON.parse(response.body)['result']['sessionName']
+    #query = URI.encode('SELECT accountname, cf_705 FROM Accounts;')  
+    #query = URI.encode("SELECT accountname FROM Accounts WHERE cf_705='#{rut}';") 
 
-    url2 = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=login'
-    digest_string = token+'cHjPQ6N4cB87PR1D'
-    md5 = Digest::MD5.hexdigest(digest_string)
-    response = RestClient.post url2, :body => {
-        'operation' => 'login',
-        'username' => user_name,
-        'accessKey' => md5
-    }
-
-
-
-    return response
-    #return "Token: " + token + " MD5: " + md5
+    #Finalmente realizamos una query para obtener la dirección de un cliente de rut #.
+    query = URI.encode("SELECT accountname,bill_street,bill_state  FROM Accounts WHERE cf_705='#{rut}';") 
+    jsonaddress = JSON.parse(RestClient.get 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=query&sessionName='+sessionid+'&query='+query)
+    address= jsonaddress['result'][0]['accountname']+', '+ jsonaddress['result'][0]['bill_street']+', '+ jsonaddress['result'][0]['bill_state']
+    return address
   end
 end
