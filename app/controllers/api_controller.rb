0d38b28331edec46ac013ceffa7d0c779a53ad48
arturonatho <arturonatho@gmail.com>
2014-05-13 16:24:57 -0400

    Agregar API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,15 @@
 class ApiController < ApplicationController
  
-  def pedirProducto
+  def pedirProducto(usuario)
+  	resultado = ""
+  	stockdisponible = true
+
+
+  	if stockdisponible = true
+
+  	else stockdisponible==false
+  		
+
+  	return resultado
   end
 end
