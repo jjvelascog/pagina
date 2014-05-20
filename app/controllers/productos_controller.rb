887b937b74a09437104247ca5352d1026e63b50b
Tania Conca <tconca92@gmail.com>
2014-05-20 17:16:19 -0400

    cronjobs

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -2,33 +2,16 @@ require "rubygems"
 
 class ProductosController < ApplicationController
   def actualizar
+  	#Producto.actualizar
+	
 
-  	require "sqlite3"
-    require 'csv'
-    require 'date'
-    
-    filepath = "#{Rails.root}/pricing/Pricing.csv"
-    text=File.open(filepath).read
-
-
-    Producto.transaction do
-
-      Producto.destroy_all
-
-      CSV.parse(text, headers: true) do |row|
-
-        f_vig= Date.strptime(row[4].strip, "%m/%d/%Y")
-        #No agrego los precios productos que no están vigente:
-        #break if f_vig < DateTime.now.strftime('%m/%d/%Y')
-        f_act= Date.strptime(row[3].strip, "%m/%d/%Y")
-
-        Producto.create!(:sku => row[1].to_i, :precio => row[2].to_i, :fechaact => f_act, :fechavig => f_vig, :costoprod => row[5].to_i, :costotras => row[6].to_i, :costoalm => row[7].to_i)
-      end
-    end
-    redirect_to(all_productos_path)
+	Producto.leeraccess
+	redirect_to(all_productos_path)
   end
 
   def index
+    #Producto.actualizar
   end
+
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Pagina::Application.initialize!
+
+#install Whenever by adding the gem to our application’s /config/environment.rb file.
+#config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
