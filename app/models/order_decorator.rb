04a009b60dc5b0df85644818621056412e10f9ef
arturonatho <arturonatho@gmail.com>
2014-06-21 13:56:33 -0400

    Conexion Almacen

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -1,3 +1,5 @@
+require "almacen.rb"
+
 module Spree
 	Order.class_eval do
 		state_machine do
@@ -15,16 +17,22 @@ module Spree
 			direccion_id = orden.ship_address_id
 			dir = Spree::Address.find(direccion_id)
 			direccion = "#{dir.firstname} #{dir.lastname}, #{dir.address1}, #{dir.city}"
+
 			puts 'direccion: ' + direccion
+			alm = Almacen.new()
+
 			for i in 0..line_todo.length-1
 				line = line_todo[i]
 				variant_id = line.variant_id
 				sku = Spree::Variant.find(variant_id).sku
-				puts 'precio: '+ line.price.to_s
-				puts 'cantidad: ' + line.quantity.to_s
-				puts 'sku: ' + sku
-				puts "==="
+				precio = line.price
+				cantidad = line.quantity
+				#puts "variant_id"+variant_id
+				#puts "sku"+sku
+				#puts cantidad
+				alm.despachar(sku, cantidad, direccion, precio,-1)
 			end
+			hola
 		end
 	end
 end
