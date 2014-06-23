b38e8fe527f73236ba027b8a9525b118bffcd075
Ignacio Campos <icampos202@gmail.com>
2014-06-23 19:33:00 -0400

    Principio grafico2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -137,7 +137,7 @@ class WelcomeController < ApplicationController
         var result = { ingreso: NumberInt(0), cantidad: NumberInt(0)};
         values.forEach(function(value) {
           result.ingreso += value.ingreso;
-          result.cantidad += values.cantidad;
+          result.cantidad += value.cantidad;
         });
         return result;
       }
@@ -148,9 +148,42 @@ class WelcomeController < ApplicationController
     bodega = Pedido_bodega.map_reduce(map2, reduce2).out(inline: true)
     
     puts "====================="
-    puts sftp.first["value"]["ingreso"]
     puts sftp.first["value"]["cantidad"]
+    puts sftp.first["value"]["ingreso"]
+    puts sftp.first
     puts "====================="
+    
+    @chart2 = LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
+      series = {
+               :type=> 'pie',
+               :name=> 'Cantidad Despachada',
+               :data=> [
+                  ['Pedidos spree',  spree.first["value"]["cantidad"]],
+                  ['Pedidos bodegas',  bodega.first["value"]["cantidad"]],
+                  {
+                     :name=> 'Pedidos ftp',    
+                     :y=> sftp.first["value"]["cantidad"],
+                     :sliced=> true,
+                     :selected=> true
+                  }
+               ]
+      }
+      f.series(series)
+      f.options[:title][:text] = "THA PIE"
+      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+      f.plot_options(:pie=>{
+        :allowPointSelect=>true, 
+        :cursor=>"pointer" , 
+        :dataLabels=>{
+          :enabled=>true,
+          :color=>"black",
+          :style=>{
+            :font=>"13px Trebuchet MS, Verdana, sans-serif"
+          }
+        }
+      })
+end
   end
 
 end
